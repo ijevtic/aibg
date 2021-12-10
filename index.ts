063b5925aeb04979fa29efc181b1faf2df5885a0
msisovic <msisovic13@gmail.com>
2021-12-10 23:20:08 +0100

    zapravo radi

--- index.ts
+++ index.ts
@@ -2,11 +2,13 @@ import fetch from 'node-fetch';
 
 const SERVER_IP = 'best2.aibg.best:9080';
 const MY_ID = 11;
-const GAME_ID = 1;
+const GAME_ID = 3;
 
-fetch(`http://${SERVER_IP}/train?gameId=${MY_ID}${GAME_ID}&playerId=${MY_ID}&position=${1}`)
-    .then(res => res.json())
-    .then(res => {
-        console.log(res);
-    });
 
+const main = async () => {
+    const inicijalnoStanje = await fetch(`http://${SERVER_IP}/train?gameId=${MY_ID}${GAME_ID}&playerId=${MY_ID}&position=${1}`)
+    .then(res => res.json());
+    console.log(inicijalnoStanje);
+}
+
+main();
