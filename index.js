12effdd071904c63c9cb525a89b5abf51a945b37
msisovic <msisovic13@gmail.com>
2021-12-10 22:23:15 +0100

    veza sa serverom

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 "use strict";
 exports.__esModule = true;
 var node_fetch_1 = require("node-fetch");
-(0, node_fetch_1["default"])('https://elephant-api.herokuapp.com/elephants')
+var SERVER_IP = 'best2.aibg.best:9080';
+var MY_ID = 11;
+var GAME_ID = 1;
+(0, node_fetch_1["default"])("http://".concat(SERVER_IP, "/train?gameId=").concat(MY_ID).concat(GAME_ID, "&playerId=").concat(MY_ID, "&position=").concat(1))
     .then(function (res) { return res.json(); })
     .then(function (res) {
     console.log(res);
--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
-import fetch from 'node-fetch'
+import fetch from 'node-fetch';
 
-fetch('https://elephant-api.herokuapp.com/elephants')
+const SERVER_IP = 'best2.aibg.best:9080';
+const MY_ID = 11;
+const GAME_ID = 1;
+
+fetch(`http://${SERVER_IP}/train?gameId=${MY_ID}${GAME_ID}&playerId=${MY_ID}&position=${1}`)
     .then(res => res.json())
     .then(res => {
         console.log(res);
